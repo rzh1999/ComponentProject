90c299ff55cc9688c503bead954b1ed0eba0799a
Bill <william.hine2019@gmail.com>
2020-08-12 16:27:15 -0400

    Refactored ApprovalCard To Display Props.Child

--- src/index.js
+++ src/index.js
@@ -7,25 +7,39 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author={faker.name.firstName()}
-        timeAgo="Today At 4:55PM"
-        commentText="Great Time"
-        avatarImg={faker.image.avatar()}
-      />
-      <CommentDetail
-        author={faker.name.firstName()}
-        timeAgo="Today At 2:00AM"
-        commentText="Awesome!!!!"
-        avatarImg={faker.image.avatar()}
-      />
-      <CommentDetail
-        author={faker.name.firstName()}
-        timeAgo="Yesterday At 5:00PM"
-        commentText="Fantastic!!!"
-        avatarImg={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="Today At 4:55PM"
+          commentText="Great Time"
+          avatarImg={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="Today At 2:00AM"
+          commentText="Awesome!!!!"
+          avatarImg={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="Yesterday At 5:00PM"
+          commentText="Fantastic!!!"
+          avatarImg={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
