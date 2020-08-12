b19b2a80f11b18e2ce629ef5f27c222ebf7431ca
Bill <william.hine2019@gmail.com>
2020-08-12 16:17:46 -0400

    Added aproval card

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import faker from "faker";
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard />
       <CommentDetail
         author={faker.name.firstName()}
         timeAgo="Today At 4:55PM"
