aa2047d874111eb32b63fbf21378bce73eb4f4a1
Bill <william.hine2019@gmail.com>
2020-08-12 16:05:36 -0400

    Made CommentDetail configurable using props

--- src/index.js
+++ src/index.js
@@ -6,7 +6,24 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo="Today At 4:55PM"
+        commentText="Great Time"
+        avatarImg={faker.image.avatar()}
+      />
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo="Today At 2:00AM"
+        commentText="Awesome!!!!"
+        avatarImg={faker.image.avatar()}
+      />
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo="Yesterday At 5:00PM"
+        commentText="Fantastic!!!"
+        avatarImg={faker.image.avatar()}
+      />
     </div>
   );
 };
